17126f5baed901c2e968542e5556c998b9e21f02
Dan <shiningm67@gmail.com>
2021-01-28 22:02:31 +0900

    Create BaekJoon P.001000

---
+++ acmicpc.net.p001000/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace acmicpc.net.p001000
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> values = new List<int>();
+
+            while (values.Count() != 2)
+            {
+                values = from item in Console.ReadLine().Split()
+                        where int.TryParse(item, out _)
+                        select int.Parse(item);
+            }
+
+            Console.WriteLine(values.Sum());
+        }
+    }
+}
