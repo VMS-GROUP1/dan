615700061a41ff2c6c11014c986d13fbefee0f2e
Dan <shiningm67@gmail.com>
2021-01-28 21:19:47 +0900

    Create BaekJoon P.002557

---
+++ acmicpc.net.p002557/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace acmicpc.net.p002557
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
