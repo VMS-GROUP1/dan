32c7e19917fe3731eadad653c28755ca2974beb6
Dan <shiningm67@gmail.com>
2021-01-28 22:35:59 +0900

    Create BaekJoon P.002558

---
+++ acmicpc.net.p002558/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace acmicpc.net.p002558
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IEnumerable<int> values = new List<int>();
+
+            while (values.Count() != 2)
+            {
+                var line1 = Console.ReadLine().Split().FirstOrDefault();
+                var line2 = Console.ReadLine().Split().FirstOrDefault();
+
+                values = from item in new string[] { line1, line2 }
+                         where int.TryParse(item, out _)
+                         select int.Parse(item);
+            }
+
+            Console.WriteLine(values.Sum());
+        }
+    }
+}
