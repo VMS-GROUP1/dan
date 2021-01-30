7ddcdc4cc73ae4c4beb750db9a00e701b505ad95
Dan <shiningm67@gmail.com>
2021-01-30 18:04:08 +0900

    Create BaekJoon P.011022

---
+++ acmicpc.net.p011022/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace acmicpc.net.p011022
+{
+    /*
+    문제
+    두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.
+
+    입력
+    첫째 줄에 테스트 케이스의 개수 T가 주어진다.
+
+    각 테스트 케이스는 한 줄로 이루어져 있으며, 각 줄에 A와 B가 주어진다. (0 < A, B < 10)
+
+    출력
+    각 테스트 케이스마다 "Case #x: A + B = C" 형식으로 출력한다. x는 테스트 케이스 번호이고 1부터 시작하며, C는 A+B이다.
+    */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+            for (int i = 0; i < count; i++)
+            {
+                var values = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+                Console.WriteLine($"Case #{i + 1}: {values[0]} + {values[1]} = {values[0] + values[1]}");
+            }
+        }
+    }
+}
