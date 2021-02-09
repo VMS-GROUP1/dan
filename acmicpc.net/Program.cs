fc6292c08f0986f904fa7af88b6f133aa6072ded
Dan <shiningm67@gmail.com>
2021-02-09 20:56:43 +0900

    문제 폴더명 변경

--- acmicpc.net/Program.cs
+++ acmicpc.net/Program.cs
@@ -9,7 +9,7 @@ namespace acmicpc.net
     class Program
     {
         const string MethodName = "Main";
-        const string FolderName = "Problems";
+        const string FolderName = "Problems.DP";
         const string StopCommand = "stop";
         const string RecentLogName = "recent.log";
 
