e3f8feacf97f2a924deac5acd6de70e9dca78fec
Dan <shiningm67@gmail.com>
2021-03-03 13:51:33 +0900

    Create P002743.cs

---
+++ acmicpc.net/Problems.Strings/P002743.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace acmicpc.net.Problems.Strings
+{
+    public class P002743
+    {
+        public static void Main(string[] args)
+        {
+            string str = Console.ReadLine();
+            Console.WriteLine(str.Length);
+        }
+    }
+}
