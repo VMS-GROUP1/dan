147a00c92e7971c4393da2aadf9ec71714abad71
Dan <shiningm67@gmail.com>
2021-03-12 00:09:39 +0900

    Create P001929.cs

---
+++ acmicpc.net/Problems.Mathematics/P001929.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace acmicpc.net.Problems.Mathematics
+{
+    public class P001929
+    {
+        public static void Main(string[] args)
+        {
+            int[] n = Console.ReadLine().Split().Select(x => int.Parse(x)).ToArray();
+            Console.WriteLine(string.Join('\n', FindPrime(n[1]).Where(x => x >= n[0])));
+        }
+        private static List<int> FindPrime(int x)
+        {
+            List<int> p = new List<int>();
+            bool[] np = new bool[x + 1];
+
+            for (int i = 2; i <= x; i++)
+            {
+                if (np[i])
+                    continue;
+
+                p.Add(i);
+                for (int j = i * i; j <= x; j += i)
+                {
+                    np[j] = true;
+                }
+            }
+
+            return p;
+        }
+    }
+}
