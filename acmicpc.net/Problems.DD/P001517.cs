fce9a3b118df7b13cc695f6af253a1f6e67727d0
Dan <shiningm67@gmail.com>
2021-04-01 12:39:05 +0900

    Update P001517.cs

--- acmicpc.net/Problems.DD/P001517.cs
+++ acmicpc.net/Problems.DD/P001517.cs
@@ -4,43 +4,72 @@ namespace acmicpc.net.Problems.DD
 {
     public class P001517
     {
-        private static int count;
+        private static long count;
         private static int[] A;
         public static void Main(string[] args)
         {
             count = 0;
             int n = int.Parse(Console.ReadLine());
             A = Array.ConvertAll(Console.ReadLine().Split(), x => int.Parse(x));
-            Sort(0);
+            Sort(0, n);
             Console.WriteLine(count);
         }
 
-        private static void Sort(int x)
+        private static void Sort(int x, int n)
         {
-            while (x + 1 < A.Length && A[x] <= A[x + 1])
-            {
-                x++;
-            }
-
-            if (x + 1 == A.Length)
+            if (n == 1)
                 return;
 
-            Swap(x);
-            Sort(0);
+            int half = n / 2;
+            Sort(x, half);
+            Sort(x + half, n - half);
+            Sort(x, half, x + half, n - half);
         }
 
-        private static void Swap(int x)
+        private static void Sort(int x, int nx, int y, int ny)
         {
-            int temp = A[x];
-            A[x] = A[x + 1];
-            A[x + 1] = temp;
-            count++;
-
-            if (x + 2 == A.Length)
+            int mid = x + nx - 1;
+            if (A[mid] <= A[y])
                 return;
 
-            if (A[x + 1] > A[x + 2])
-                Swap(x + 1);
+            if (nx == 0 || ny == 0)
+                return;
+
+            if (nx == 1 && ny == 1)
+            {
+                if (A[x] > A[y])
+                {
+                    count++;
+                    int t = A[x];
+                    A[x] = A[y];
+                    A[y] = t;
+                }
+                return;
+            }
+
+            int i = x;
+            int j = y;
+            int k = 0;
+            int end = y + ny - 1;
+            int[] temp = new int[nx + ny];
+            while (i <= mid || j <= end)
+            {
+                if (i <= mid && (j > end || A[i] <= A[j]))
+                {
+                    temp[k++] = A[i++];
+                }
+                else
+                {
+                    count += mid - i + 1;
+                    temp[k++] = A[j++];
+                }
+            }
+
+            i = 0;
+            while (i < temp.Length)
+            {
+                A[i + x] = temp[i++];
+            }
         }
     }
 }
