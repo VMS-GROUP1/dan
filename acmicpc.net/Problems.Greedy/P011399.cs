8c7d89d926daece66d0ccdf424f67e0a5b4485d4
Dan <shiningm67@gmail.com>
2021-04-03 16:32:01 +0900

    Update P011399.cs

--- acmicpc.net/Problems.Greedy/P011399.cs
+++ acmicpc.net/Problems.Greedy/P011399.cs
@@ -7,13 +7,13 @@ namespace acmicpc.net.Problems.Greedy
     {
         public static void Main(string[] args)
         {
-            short n = short.Parse(Console.ReadLine());
-            short[] time = Array.ConvertAll(Console.ReadLine().Split(), x => short.Parse(x));
+            int n = int.Parse(Console.ReadLine());
+            int[] time = Array.ConvertAll(Console.ReadLine().Split(), x => int.Parse(x));
             Array.Sort(time);
 
             int ans = 0;
             int wait = 0;
-            short i = 0;
+            int i = 0;
             while (i < n)
             {
                 wait += time[i++];
