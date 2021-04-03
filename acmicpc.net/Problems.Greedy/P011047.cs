2a44e39d93770564b16f8b744e66fa2d562dc666
Dan <shiningm67@gmail.com>
2021-04-03 11:56:20 +0900

    Update P011047.cs

--- acmicpc.net/Problems.Greedy/P011047.cs
+++ acmicpc.net/Problems.Greedy/P011047.cs
@@ -8,16 +8,19 @@ namespace acmicpc.net.Problems.Greedy
         {
             int[] data = Array.ConvertAll(Console.ReadLine().Split(), x => int.Parse(x));
             int n = data[0];
+            int k = data[1];
             int[] coin = new int[n];
+            int max = 0;
 
             while (n > 0)
             {
                 coin[--n] = int.Parse(Console.ReadLine());
+                if (coin[n] <= k)
+                    max = n;
             }
 
             int ans = 0;
-            int k = data[1];
-            n = 0;
+            n = max;
             while (k > 0)
             {
                 int count = k / coin[n];
