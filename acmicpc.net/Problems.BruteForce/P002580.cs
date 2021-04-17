8a79483fdab028ec2528e60a55cb2454eb4641f2
Dan <shiningm67@gmail.com>
2021-04-17 14:37:31 +0900

    Update P002580.cs

--- acmicpc.net/Problems.BruteForce/P002580.cs
+++ acmicpc.net/Problems.BruteForce/P002580.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace acmicpc.net.Problems.BruteForce
@@ -8,25 +9,19 @@ namespace acmicpc.net.Problems.BruteForce
     {
         private static int[][] M;
         private static int[,] R;
-        private static int[] SumR;
         private static int[,] C;
-        private static int[] SumC;
         private static int[,] S;
-        private static int[] SumS;
-        private static int[] T;
-        private static int SumT;
+        private static List<(int r, int c)> Waiting = new List<(int r, int c)>();
+        private static bool Found;
 
         public static void Main(string[] args)
         {
             M = new int[9][];
             R = new int[9, 10];
-            SumR = new int[9];
             C = new int[9, 10];
-            SumC = new int[9];
             S = new int[9, 10];
-            SumS = new int[9];
-            T = new int[10];
-            SumT = 0;
+            Waiting = new List<(int r, int c)>();
+            Found = false;
             for (int i = 0; i < 9; i++)
             {
                 M[i] = Array.ConvertAll(Console.ReadLine().Split(), x => int.Parse(x));
@@ -38,141 +33,67 @@ namespace acmicpc.net.Problems.BruteForce
                 {
                     int value = M[i][j];
                     R[i, value] = 1;
-                    SumR[i] += value;
                     C[j, value] = 1;
-                    SumC[j] += value;
                     S[SmallIndex(i, j), value] = 1;
-                    SumS[SmallIndex(i, j)] += value;
-                    T[value] += 1;
-                    SumT += value;
+
+                    if (value == 0)
+                        Waiting.Add((i, j));
                 }
             }
 
-            Do(0, 0, 1);
+            Do(0);
+
+            for (int i = 0; i < M.Length; i++)
+            {
+                Console.WriteLine(string.Join(' ', M[i]));
+            }
         }
 
-        private static void Do(int r, int c, int x)
+        private static void Do(int level)
         {
-            if (SumT == 405)
-                return;
+            var cell = Waiting[level];
+            int r = cell.r;
+            int c = cell.c;
 
-            if (M[r][c] != 0)
+            for (int i = 1; i <= 9; i++)
             {
-                if (r + 1 < M.Length)
-                    Do(r + 1, c, x);
-                if (c + 1 < M[0].Length)
-                    Do(r, c + 1, x);
+                if (Check(r, c, i))
+                {
+                    M[r][c] = i;
+                    R[r, i] = 1;
+                    C[c, i] = 1;
+                    S[SmallIndex(r, c), i] = 1;
 
-                return;
-            }
+                    if (level == Waiting.Count - 1)
+                    {
+                        Found = true;
+                        break;
+                    }
 
-            if (x >= 1 && x <= 9 && Check(r, c, x))
-            {
-                M[r][c] = x;
-                R[r, x] = 1;
-                C[c, x] = 1;
-                S[SmallIndex(r, c), x] = 1;
-                T[x] += 1;
-                SumR[r] += x;
-                SumC[c] += x;
-                SumT += x;
-                Do(r + 1, c, x);
-                Do(r + 1, c + 1, x);
-                R[r, x] = 0;
-                C[c, x] = 0;
-                S[SmallIndex(r, c), x] = 0;
-                T[x] -= 1;
-                SumR[r] -= x;
-                SumC[c] -= x;
-                SumT -= x;
-                M[r][c] = 0;
-            }
-            else
-            {
-                Do(r, c, x + 1);
-                Do(r, c, x - 1);
+                    Do(level + 1);
+                    if (Found)
+                        break;
+
+                    M[r][c] = 0;
+                    R[r, i] = 0;
+                    C[c, i] = 0;
+                    S[SmallIndex(r, c), i] = 0;
+                }
             }
         }
-
-        // private static void Do(int r, int c, int x)
-        // {
-        //     if (SumT == 405)
-        //         return;
-
-        //     if (M[r][c] != 0)
-        //     {
-        //         if (r + 1 < M.Length)
-        //             Do(r + 1, c, 0);
-        //         if (c + 1 < M[0].Length)
-        //             Do(r, c + 1, 0);
-
-        //         return;
-        //     }
-
-        //     for (int i = 1; i < 9; i++)
-        //     {
-        //         if (Check(r, c, i))
-        //         {
-        //             M[r][c] = i;
-        //             R[r, i] = 1;
-        //             C[c, i] = 1;
-        //             S[SmallIndex(r, c), i] = 1;
-        //             T[i] += 1;
-        //             SumR[r] += i;
-        //             SumC[c] += i;
-        //             SumT += i;
-        //             Do(r, c, i);
-        //             R[r, i] = 0;
-        //             C[c, i] = 0;
-        //             S[SmallIndex(r, c), i] = 0;
-        //             T[i] -= 1;
-        //             SumR[r] -= i;
-        //             SumC[c] -= i;
-        //             SumT -= i;
-        //             M[r][c] = 0;
-        //         }
-        //         // if (R[r, i] == 0 && SumR[r] + i == 45)
-        //         // {
-        //         //     M[r][c] = i;
-        //         //     SumR[r] += i;
-        //         //     SumT += i;
-        //         //     Do(r, c, i);
-        //         //     SumR[r] -= i;
-        //         //     M[r][c] = 0;
-        //         // }
-
-        //         // if (C[c, i] == 0 && SumC[c] + i == 45)
-        //         // {
-        //         //     M[r][c] = i;
-        //         //     SumC[c] += i;
-        //         //     Do(r, c, i);
-        //         //     SumC[c] -= i;
-        //         //     M[r][c] = 0;
-        //         // }
-
-        //         // int s = SmallIndex(r, c);
-        //         // if (S[s, i] == 0 && SumS[s] + i == 45)
-        //         // {
-        //         //     M[r][c] = i;
-        //         //     SumS[s] += i;
-        //         //     Do(r, c, i);
-        //         //     SumS[s] -= i;
-        //         //     M[r][c] = 0;
-        //         // }
-        //     }
-        // }
-
 
         private static bool Check(int r, int c, int value)
         {
-            if (R[r, value] == 0 && SumR[r] + value == 45)
-                return true;
-            if (C[c, value] == 0 && SumC[c] + value == 45)
-                return true;
-            if (S[SmallIndex(r, c), value] == 0 && SumS[SmallIndex(r, c)] + value == 45)
-                return true;
+            if (R[r, value] == 1)
+                return false;
+            if (C[c, value] == 1)
+                return false;
 
-            return false;
+            int sIndex = SmallIndex(r, c);
+            if (S[sIndex, value] == 1)
+                return false;
+
+            return true;
         }
 
         private static int SmallIndex(int r, int c)
