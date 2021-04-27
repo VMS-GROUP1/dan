335d0c959e10c6725a16ab58dff5951a738a2491
Dan <shiningm67@gmail.com>
2021-04-27 22:57:50 +0900

    Create P010171.cs

---
+++ acmicpc.net/Problems.IO/P010171.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace acmicpc.net.Problems.IO
+{
+    public class P010171
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("\\    /\\");
+            Console.WriteLine(" )  ( ')");
+            Console.WriteLine("(  /  )");
+            Console.WriteLine(" \\(__)|");
+        }
+    }
+}
