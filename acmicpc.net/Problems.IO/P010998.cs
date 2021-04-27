6d6758e1086a7d875f20c032e185d2979ec94c4e
Dan <shiningm67@gmail.com>
2021-04-27 23:06:26 +0900

    Create P010998.cs

---
+++ acmicpc.net/Problems.IO/P010998.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace acmicpc.net.Problems.IO
+{
+    public class P010998
+    {
+        public static void Main(string[] args)
+        {
+            int[] p = Array.ConvertAll(Console.ReadLine().Split(), x => int.Parse(x));
+            Console.WriteLine(p[0] * p[1]);
+        }
+    }
+}
