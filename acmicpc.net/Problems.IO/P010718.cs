6a9ad0ef436ab944f4734cd1cdbb7549dfd7e042
Dan <shiningm67@gmail.com>
2021-04-27 22:52:35 +0900

    Create P010718.cs

---
+++ acmicpc.net/Problems.IO/P010718.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace acmicpc.net.Problems.IO
+{
+    public class P010718
+    {
+        public static void Main(string[] args)
+        {
+            string message = "강한친구 대한육군\n강한친구 대한육군";
+            Console.WriteLine(message);
+        }
+    }
+}
