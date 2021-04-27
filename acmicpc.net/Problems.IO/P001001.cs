55ebda406637c544fc1582c5c21ebc7e836bca8a
Dan <shiningm67@gmail.com>
2021-04-27 23:04:25 +0900

    Create P001001.cs

---
+++ acmicpc.net/Problems.IO/P001001.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace acmicpc.net.Problems.IO
+{
+    //A-B
+    public class P001001
+    {
+        public static void Main(string[] args)
+        {
+            int[] p = Array.ConvertAll(Console.ReadLine().Split(), x => int.Parse(x));
+            Console.WriteLine(p[0] - p[1]);
+        }
+    }
+}
